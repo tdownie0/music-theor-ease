bbe43fe5e97963461dfc182498149d80425a506d
duncan06 <tylerdownie0@gmail.com>
2024-01-24 20:48:30 -0800

    Add variables for readability.

--- app/components/CircleOfFifths.tsx
+++ app/components/CircleOfFifths.tsx
@@ -1,9 +1,16 @@
-'use client'
+"use client";
 import React, { useEffect, useState } from "react";
 import { Note, getCircleOfFifthsArrayAsync } from "../utils/musicLogic";
 
+const sizeOfCircle: number = 150;
+const radiusOfCircle: number = 100;
+const radiusOutsideOfCircle: number = 20;
+const placementRadius: number = radiusOfCircle + radiusOutsideOfCircle;
+const degreesToRadians: number = Math.PI / 180;
+const angleIncrement: number = 360 / 12;
+
 const CircleOfFifths: React.FC = () => {
-    const [circleOfFifthsArray, setCircleOfFifthsArray] = useState<Note[]>([]);
+  const [circleOfFifthsArray, setCircleOfFifthsArray] = useState<Note[]>([]);
 
   useEffect(() => {
     // Fetch data asynchronously and update state
@@ -27,27 +34,25 @@ const CircleOfFifths: React.FC = () => {
       />
 
       {/* Text labels for each key on the outer rim */}
-      {circleOfFifthsArray.map(
-        (note, index) => {
-          const angle = (index - 3) * (360 / 12) * (Math.PI / 180);
-          const x = 150 + 120 * Math.cos(angle);
-          const y = 150 + 120 * Math.sin(angle);
+      {circleOfFifthsArray.map((note, index) => {
+        const angle = (index - 3) * angleIncrement * degreesToRadians;
+        const x = sizeOfCircle + placementRadius * Math.cos(angle);
+        const y = sizeOfCircle + placementRadius * Math.sin(angle);
 
-          return (
-            <text
-              key={index}
-              x={x}
-              y={y}
-              fill="text-primary"
-              className="text-primary" // Use inline style with dynamic color class
-              textAnchor="middle"
-              alignmentBaseline="middle"
-            >
-              {note}
-            </text>
-          );
-        }
-      )}
+        return (
+          <text
+            key={index}
+            x={x}
+            y={y}
+            fill="text-primary"
+            className="text-primary" // Use inline style with dynamic color class
+            textAnchor="middle"
+            alignmentBaseline="middle"
+          >
+            {note}
+          </text>
+        );
+      })}
     </svg>
   );
 };
