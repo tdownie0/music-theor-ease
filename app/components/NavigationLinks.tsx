56227f741789119f11571b138bfd4152ece75db4
duncan06 <tylerdownie0@gmail.com>
2024-02-10 16:26:39 -0800

    Refactor.

--- app/components/NavigationLinks.tsx
+++ app/components/NavigationLinks.tsx
@@ -6,22 +6,15 @@ interface NavigationLinksProps {
   linkType?: siteLinks;
 }
 
-const NavigationLinks: React.FC<NavigationLinksProps> = ({ linkType }) => {
-  let linkNames: siteLinks[] = [];
+const linkNames: Record<siteLinks, siteLinks[]> = {
+  [siteLinks.Home]: [siteLinks.Circles, siteLinks.Modes, siteLinks.Quiz],
+  [siteLinks.Circles]: [siteLinks.Home, siteLinks.Modes, siteLinks.Quiz],
+  [siteLinks.Modes]: [siteLinks.Home, siteLinks.Circles, siteLinks.Quiz],
+  [siteLinks.Quiz]: [siteLinks.Home, siteLinks.Circles, siteLinks.Modes],
+};
 
-  switch (linkType) {
-    case siteLinks.Circles:
-      linkNames = [siteLinks.Home, siteLinks.Modes, siteLinks.Quiz];
-      break;
-    case siteLinks.Modes:
-      linkNames = [siteLinks.Home, siteLinks.Circles, siteLinks.Quiz];
-      break;
-    case siteLinks.Quiz:
-      linkNames = [siteLinks.Home, siteLinks.Circles, siteLinks.Modes];
-      break;
-    default:
-      linkNames = [siteLinks.Circles, siteLinks.Modes, siteLinks.Quiz];
-  }
+const NavigationLinks: React.FC<NavigationLinksProps> = ({ linkType }) => {
+  const linksToRender = linkType ? linkNames[linkType] : linkNames[siteLinks.Home];
 
   const renderLink = (linkName: siteLinks, index: number): JSX.Element => {
     const isHomePage = linkName === siteLinks.Home;
@@ -41,7 +34,7 @@ const NavigationLinks: React.FC<NavigationLinksProps> = ({ linkType }) => {
 
   return (
     <ul className="flex">
-      {linkNames.map((linkName, index) => renderLink(linkName, index))}
+      {linksToRender.map((linkName, index) => renderLink(linkName, index))}
     </ul>
   );
 };
