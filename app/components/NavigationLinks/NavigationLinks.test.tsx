a9744e933023286d2a052dfacb5c3ca024c2eb8d
duncan06 <tylerdownie0@gmail.com>
2024-02-12 15:31:14 -0800

    Refactor test.

--- app/components/NavigationLinks/NavigationLinks.test.tsx
+++ app/components/NavigationLinks/NavigationLinks.test.tsx
@@ -6,8 +6,8 @@ import NavigationLinks from "./NavigationLinks";
 import { siteLinks } from "../../utils/enums";
 
 describe("NavigationLinks component", () => {
-  test("renders links correctly with different linkTypes", () => {
-    Object.values(siteLinks).forEach((link: siteLinks) => {
+  Object.values(siteLinks).forEach((link: siteLinks) => {
+    it(`renders links correctly with linkType=${link}`, () => {
       render(<NavigationLinks linkType={link} />);
 
       Object.values(siteLinks).forEach((otherLink) => {
@@ -18,6 +18,7 @@ describe("NavigationLinks component", () => {
 
       const links = screen.getAllByRole("link");
       expect(links).toHaveLength(3);
+
       cleanup();
     });
   });
@@ -25,12 +26,15 @@ describe("NavigationLinks component", () => {
   test("renders links with correct paths and hover colors", () => {
     render(<NavigationLinks />);
 
-    const circlesLink = screen.getByText("Circles");
-    expect(circlesLink).toHaveAttribute("href", "/circles");
-    expect(circlesLink).toHaveClass("hover:text-primary");
-
-    const modesLink = screen.getByText("Modes");
-    expect(modesLink).toHaveAttribute("href", "/modes");
-    expect(modesLink).toHaveClass("hover:text-secondary");
+    const links = screen.getAllByRole("link");
+    const hoverColors = [
+      "hover:text-primary",
+      "hover:text-secondary",
+      "hover:text-accent",
+    ];
+    
+    expect(links[0]).toHaveClass(hoverColors[0]);
+    expect(links[1]).toHaveClass(hoverColors[1]);
+    expect(links[2]).toHaveClass(hoverColors[2]);
   });
 });
