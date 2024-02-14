e74d468984dc5bcac61b7dbf2e1c440cda46e31a
duncan06 <tylerdownie0@gmail.com>
2024-02-13 20:25:19 -0800

    Remove extra comments.

--- app/components/DreamBackground/DreamBackground.cy.tsx
+++ app/components/DreamBackground/DreamBackground.cy.tsx
@@ -1,33 +1,26 @@
-// cypress/integration/components/DreamBackground.spec.ts
 import DreamBackground from "./DreamBackground";
 
 describe("DreamBackground component", () => {
   let canvas: HTMLCanvasElement;
 
   beforeEach(() => {
-    // Create a canvas element
     canvas = document.createElement("canvas");
     canvas.setAttribute("data-testid", "dream-background");
 
     document.body.appendChild(canvas);
 
-    // Mount the DreamBackground component with the canvas element
     cy.mount(<DreamBackground showCanvas={true} canvasElement={canvas} />);
   });
 
   it("should display the DreamBackground component", () => {
-    // Assert that the DreamBackground component is visible on the page
     cy.get('[data-testid="dream-background"]').should("be.visible");
   });
 
   it("should animate the circles", () => {
-    // Take screenshot before animation
     cy.screenshot("before-animation");
 
-    // Wait for animation to complete
-    cy.wait(2000); // Adjust wait time as needed
+    cy.wait(2000); 
 
-    // Take screenshot after animation
     cy.screenshot("after-animation");
 
     // Compare screenshots
