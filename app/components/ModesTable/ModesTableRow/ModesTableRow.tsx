1c1653bed596070cc2c65a07341e12fde331e5a3
duncan06 <tylerdownie0@gmail.com>
2024-02-21 16:15:04 -0800

    Refactor.

--- app/components/ModesTable/ModesTableRow/ModesTableRow.tsx
+++ app/components/ModesTable/ModesTableRow/ModesTableRow.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-interface ModesTableRowProps {
+type ModesTableRowProps = {
   modeName?: string;
   modeArray?: string[];
   isHeaderRow?: boolean;
@@ -16,48 +16,48 @@ const ModesTableRow: React.FC<ModesTableRowProps> = ({
   modeArray,
   isHeaderRow,
 }) => {
-  if (isHeaderRow) {
-    return (
-      <tr className="bg-primary text-primary-content text-center">
-        <th
-          key="Mode"
-          className={cellStyles}
-          style={{ width: modeNameColumnWidth }}
-        >
-          Mode
-        </th>
-        {[...Array(amountOfIntervals)].map((_, index) => (
-          <th
-            key={index}
-            className={cellStyles}
-            style={{ width: intervalColumnWidth }}
-          >
-            {index + 1}
-          </th>
-        ))}
-      </tr>
-    );
-  }
-
   return (
-    <tr className="text-center">
-      <td
-        key={modeName}
-        className={cellStyles}
-        style={{ width: modeNameColumnWidth }}
-      >
-        {modeName}
-      </td>
-      {modeArray?.map((note, noteIndex) => (
-        <td
-          key={noteIndex}
-          className={cellStyles}
-          style={{ width: intervalColumnWidth }}
-        >
-          {note}
-        </td>
-      ))}
-    </tr>
+    <>
+      {isHeaderRow ? (
+        <tr className="bg-primary text-primary-content text-center">
+          <th
+            key="Mode"
+            className={cellStyles}
+            style={{ width: modeNameColumnWidth }}
+          >
+            Mode
+          </th>
+          {[...Array(amountOfIntervals)].map((_, index) => (
+            <th
+              key={index}
+              className={cellStyles}
+              style={{ width: intervalColumnWidth }}
+            >
+              {index + 1}
+            </th>
+          ))}
+        </tr>
+      ) : (
+        <tr className="text-center">
+          <td
+            key={modeName}
+            className={cellStyles}
+            style={{ width: modeNameColumnWidth }}
+          >
+            {modeName}
+          </td>
+          {modeArray?.map((note, noteIndex) => (
+            <td
+              key={noteIndex}
+              className={cellStyles}
+              style={{ width: intervalColumnWidth }}
+            >
+              {note}
+            </td>
+          ))}
+        </tr>
+      )}
+    </>
   );
 };
 
