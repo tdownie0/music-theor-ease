e5fecb47a127086ecca8e88d3deb470e38e8533f
duncan06 <tylerdownie0@gmail.com>
2024-02-22 18:01:28 -0800

    Format.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React from "react";
 import NavigationLinks from "./components/NavigationLinks/NavigationLinks";
 
 const Home: React.FC = () => {
@@ -6,7 +6,9 @@ const Home: React.FC = () => {
     <div className="primary min-h-screen mt-16 ml-4">
       <NavigationLinks />
       <h1 className="text-4xl font-bold mt-8">Music Theor-ease</h1>
-      <div className="mt-8 text-lg">Introduction to the pillars of music concepts</div>
+      <div className="mt-8 text-lg">
+        Introduction to the pillars of music concepts
+      </div>
     </div>
   );
 };
