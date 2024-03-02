e7f5c61d8c2f7ceae2e8d2a414eb72bc4e03c6bb
duncan06 <tylerdownie0@gmail.com>
2024-03-01 17:47:26 -0800

    Add styling and more dragging and dropping functionality.

--- app/components/CircleQuiz/CircleQuiz.tsx
+++ app/components/CircleQuiz/CircleQuiz.tsx
@@ -7,6 +7,7 @@ import QuizTile from "./QuizTile/QuizTile";
 
 const CircleQuiz = () => {
   const [notes, setNotes] = useState<allNotes[]>([]);
+  const [draggedTileIndex, setDraggedTileIndex] = useState<number | null>(null);
 
   useEffect(() => {
     async function getCircleNotes(): Promise<void> {
@@ -26,30 +27,51 @@ const CircleQuiz = () => {
     updatedNotes.splice(dragIndex, 1);
     updatedNotes.splice(hoverIndex, 0, draggedTile);
     setNotes(updatedNotes);
+    setDraggedTileIndex(hoverIndex); // Update the index of the currently dragged tile
   };
 
   return (
     <DndProvider backend={HTML5Backend}>
-      <div className="flex flex-col gap-4">
-        <div className="flex w-full justify-center gap-1">
-          {notes.slice(0, Math.ceil(notes.length / 2)).map((note, index) => (
-            <QuizTile
-              key={index}
-              index={index}
-              note={note}
-              moveTile={moveTile}
-            />
-          ))}
+      <div className="flex flex-col gap-4 rounded-lg shadow-lg bg-base-300 p-8 w-full">
+        <div className="flex w-full justify-end items-center">
+          <p className="text-xl font-medium w-full">
+            Arrange into the circle of fifths
+          </p>
+          <button className="btn-secondary btn-circle btn w-1/5">Reset</button>
         </div>
-        <div className="flex w-full justify-center gap-1">
-          {notes.slice(Math.ceil(notes.length / 2)).map((note, index) => (
-            <QuizTile
-              key={index}
-              index={index + Math.ceil(notes.length / 2)}
-              note={note}
-              moveTile={moveTile}
-            />
-          ))}
+        <div className="bg-secondary flex flex-col w-full justify-center gap-4 p-8 rounded-lg">
+          <div className="flex gap-4 bg-primary p-2 rounded-lg">
+            {notes.slice(0, Math.ceil(notes.length / 2)).map((note, index) => (
+              <div className="bg-accent p-2 rounded-md">
+                <QuizTile
+                  key={index}
+                  index={index}
+                  note={note}
+                  moveTile={moveTile}
+                  isDragging={draggedTileIndex === index}
+                />
+              </div>
+            ))}
+          </div>
+          <div className="flex gap-4 bg-primary p-2 rounded-lg">
+            {notes.slice(Math.ceil(notes.length / 2)).map((note, index) => (
+              <div className="bg-accent p-2 rounded-md">
+                <QuizTile
+                  key={index}
+                  index={index + Math.ceil(notes.length / 2)}
+                  note={note}
+                  moveTile={moveTile}
+                  isDragging={
+                    draggedTileIndex === index + Math.ceil(notes.length / 2)
+                  }
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex w-full justify-end">
+          <button className="btn-primary btn-square btn w-1/5">Check</button>
         </div>
       </div>
     </DndProvider>
--- app/components/CircleQuiz/QuizTile/QuizTile.tsx
+++ app/components/CircleQuiz/QuizTile/QuizTile.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useDrag, useDrop } from "react-dnd";
 import { allNotes } from "../../../utils/musicLogic";
 
@@ -6,24 +6,35 @@ type QuizTileProps = {
   note: allNotes;
   index: number;
   moveTile: (dragIndex: number, hoverIndex: number) => void;
+  isDragging: boolean;
 };
 
-const QuizTile: React.FC<QuizTileProps> = ({ note, index, moveTile }) => {
-  const [{ isDragging }, drag] = useDrag({
+const QuizTile: React.FC<QuizTileProps> = ({
+  note,
+  index,
+  moveTile,
+  isDragging,
+}) => {
+  const tileRef = useRef<HTMLDivElement>(null);
+  const [, drag] = useDrag({
     type: "QuizTile",
     item: { note, index },
     collect: (monitor) => ({
       isDragging: monitor.isDragging(),
     }),
+    end: (item, monitor) => {
+      if (!monitor.didDrop()) {
+        const elements = document.querySelectorAll(".animate-bounce");
+        elements.forEach((element) => {
+          element.classList.remove("animate-bounce");
+        });
+      }
+    },
   });
 
   const [, drop] = useDrop({
     accept: "QuizTile",
-    hover(item: { index: number }, monitor) {
-      if (!item) {
-        return;
-      }
-
+    hover(item: { index: number }) {
       const dragIndex = item.index;
       const hoverIndex = index;
 
@@ -34,11 +45,22 @@ const QuizTile: React.FC<QuizTileProps> = ({ note, index, moveTile }) => {
       moveTile(dragIndex, hoverIndex);
       item.index = hoverIndex;
     },
+    drop: () => {
+      if (tileRef.current) {
+        tileRef.current.classList.remove("animate-bounce");
+      }
+    },
   });
 
   return (
-    <div ref={(node) => drag(drop(node))} >
-      <div className="flex justify-center rounded-lg shadow-lg bg-base-300 p-4 w-14">
+    <div ref={(node) => drag(drop(node))}>
+      <div
+        ref={tileRef}
+        className={`flex justify-center btn-primary btn text-primary-content w-14 hover:cursor-grab active:cursor-grabbing ${
+          isDragging ? "animate-bounce" : ""
+        }`}
+        style={{ borderRadius: "8px" }}
+      >
         {note}
       </div>
     </div>
